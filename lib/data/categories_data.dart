f19066c6e797936fe294f2e3e9154f34f271a45f
Infant Ajay Venus <infantcsajay@outlook.com>
2023-09-05 16:43:50 +0530

    [data] chore: add category data

---
+++ lib/data/categories_data.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+List<Category> availableCategories = [
+  Category(
+    title: 'Italia',
+    color: Colors.purple,
+  ),
+  Category(
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+  Category(
+    title: 'Vegan',
+    color: Colors.yellow,
+  ),
+  Category(
+    title: 'Vegetarian',
+    color: Colors.greenAccent,
+  ),
+  Category(
+    title: 'Meat',
+    color: Colors.brown,
+  ),
+  Category(
+    title: 'Seafood',
+    color: Colors.indigo,
+  ),
+];
