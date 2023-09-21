5dbe1bd2d4fe17d3c92b0227100c83a9bd93d013
Infant Ajay Venus <infantcsajay@outlook.com>
2023-09-21 08:29:24 +0530

    [meal_info] feat: build eal_info page with info

--- lib/screens/meal_info.dart
+++ lib/screens/meal_info.dart
@@ -9,9 +9,54 @@ class MealInfo extends StatelessWidget {
   build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(mealData.title),
+        title: Text(
+          mealData.title,
+          overflow: TextOverflow.ellipsis,
+        ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              mealData.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+            const SizedBox(height: 16),
+            for (final ingredient in mealData.ingredients)
+              Text(
+                ingredient,
+              ),
+            const SizedBox(height: 24),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+            const SizedBox(height: 16),
+            for (final step in mealData.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
